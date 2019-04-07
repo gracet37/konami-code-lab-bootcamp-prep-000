bfd63c0c70b133b984425af68ddc8746e187b171
Grace Tan <gracetan37@gmail.com>
2019-04-07 07:08:11 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -23,20 +23,24 @@ const codes = [
 
 
 function init(e) {
-  var codePosition = 0
-  document.body.addEventListener('keydown',function(e) {
-    var key = allowedKeys[e.keyCode];
-    var requiredKey = codes[codePosition];
-    if(key === requiredKey) {
-      codePosition++;
-      if(codePosition === codes.length) {
-        alert("YAY");
-        codePosition = 0;
-      }
-    } else {
-      codePosition = 0;
-    }
-  })
+var konamiCodePosition = 0;
+// add keydown event listener
+document.addEventListener('keydown', function(e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+  // compare the key with the required key
+  if (key == requiredKey) {
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length)
+      activateCheats();
+  } else
+    konamiCodePosition = 0;
+});
+  
 }
 
 init(); 

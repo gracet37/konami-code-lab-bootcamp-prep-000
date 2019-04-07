2a38446fb5d4bbb520133a29ea7c366eba821c41
Grace Tan <gracetan37@gmail.com>
2019-04-07 06:31:54 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+
+let i = 0;
+
+function init(e) {
+ const body = document.querySelector("body")
+
+ body.addEventListener('keydown',function(e) {
+   
+   const key = e.keydown;
+   if(key === codes[i]) {
+     i++;
+     
+     if(i === codes.length) {
+       alert("Woohoo");
+       
+       i = 0
+     }
+     
+   } else {
+     i = 0;
+   }
+ })
 }
+ 
